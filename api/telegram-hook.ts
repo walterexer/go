0d992a2d0e5f0a1a4b11b3f703a61358ac033589
walterexer <walterexer.9@gmail.com>
2026-08-16 20:25:47 +0000

    Update telegram-hook.ts

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -2,42 +2,36 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { Telegraf } from "telegraf";
 
 // Environment variables
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const webhookUrl = process.env.WEBHOOK_URL;
-
-// Go to https://<url>/api/telegram-hook?setWebhook=true to set bot webhook url
+const BOT_TOKEN = process.env.BOT_TOKEN; // Replace with your bot token
+///api.telegram.org/bot{token}/setWebhook?url={url}/api/telegram-hook?secret_hash=32e58fbahey833349df3383dc910e180
+//api.telegram.org/bot{token}setWebhook?url=https://mobile-proxies.vercel.app/api/telegram-hook?secret_hash=32e58fbahey833349df3383dc910e180
 
 const bot = new Telegraf(BOT_TOKEN);
 
 // Handle the /start command
 export async function handleStartCommand(ctx) {
   const COMMAND = "/start";
+  const channelUrl = "t.me/apexxxhpns";
+  const targetUrl = "t.me/+gYHVARxTG7oyNmM8";
 
   // Welcome message with Markdown formatting
   const reply = `
-Unlock 100% Free VPN Access — No Limits, No Trials
-
-Enjoy fast, secure, and private VPN connections with zero cost.
-No sign-ups. No restrictions.
-
-Instantly connect to global servers
-
-Stay protected on public Wi-Fi and keep your data safe
-
-High-speed performance for smooth browsing
-
-Works on all devices — anytime, anywhere
-
-Ready to browse without borders? Get today's list below
- `;
+[Your gateway to rising higher, thinking bigger, and moving faster toward success,
+offering elite insights, high energy motivation, and wealth-driven strategies for those who refuse to settle,
+with all 2026 paying methods posted for free. Banklogs, cc methods, cashapp flips, linkables etc all for free!](${targetUrl})
+`;
 
   try {
     await ctx.reply(reply, {
       parse_mode: "Markdown",
       reply_markup: {
         inline_keyboard: [
-          [{ text: "Get Today's Socks5", callback_data: "socks_5" }],
-          [{ text: "Get Today's Socks4", callback_data: "socks_4" }],
+          [
+            {
+              text: "Join Apex VPNS & Proxies",
+              url: channelUrl,
+            },
+          ]
         ],
       },
     });
@@ -46,26 +40,28 @@ Ready to browse without borders? Get today's list below
     console.error(`Something went wrong with the ${COMMAND} command:`, error);
   }
 }
-
-// Socks 5
-bot.action("socks_5", async (ctx) => {
-  await ctx.answerCbQuery();
-  await ctx.replyWithDocument({
-    url: "https://github.com/emerur/unlimited_bot/blob/main/socks5.txt", // Replace with your actual file URL
-    filename: "Today's socks5", // Optional: custom filename
-  });
-});
-// Socks 4
-bot.action("socks_4", async (ctx) => {
-  await ctx.answerCbQuery();
-  await ctx.replyWithDocument({
-    url: "https://github.com/emerur/unlimited_bot/blob/main/socks4.txt", // Replace with your actual file URL
-    filename: "Today's socks4", // Optional: custom filename
-  });
-});
+export async function sendImageCommand(ctx) {
+  const media = [
+    {
+      type: "photo",
+      media:
+        "https://raw.githubusercontent.com/walterexer/go/main/photo_5765143904779964427_y.jpg",
+    },
+    {
+      type: "photo",
+      media:
+        "https://raw.githubusercontent.com/walterexer/go/main/photo_6050598181575266289_y.jpg",
+    }
+    
+  ];
+  // Send image first
+  await ctx.replyWithMediaGroup(media);
+}
 
 // Register the /start command handler
 bot.command("start", async (ctx) => {
+  // Send image first
+  await sendImageCommand(ctx);
   await handleStartCommand(ctx);
 });
 
@@ -75,6 +71,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     const { body, query } = req;
 
     if (query.setWebhook === "true") {
+      const webhookUrl = `${process.env.WEBHOOK_URL}`;
       const success = await bot.telegram.setWebhook(webhookUrl);
       // console.log("Webchook set:", webhookUrl, success);
       return res.status(200).send("OK");
